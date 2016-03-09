efd273c1b84d4500cea8b983c669a0c50720c37e
Stanislav Yotov <yotov.st@gmail.com>
2016-03-09 12:59:40 +0000

    initial commit

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -6,6 +6,11 @@ import java.util.Random;
 public class BubbleSort<E extends Comparable<E>> {
 	private E[] a;
 
+	/**
+	 * Bubble sort implementation
+	 * @param c the class type
+	 * @param s the size of the array we wish to create
+	 */
 	public BubbleSort(Class<E> c, int s) {
 		// Use Array native method to create array
 		// of a type only known at run time
@@ -13,59 +18,61 @@ public class BubbleSort<E extends Comparable<E>> {
 		final E[] a = (E[]) Array.newInstance(c, s);
 		this.a = a;
 	}
-	
-	public static void main(String[] args){
-		int numElements = (int) Math.pow(10,3);
-		BubbleSort<Integer> bs = new BubbleSort<Integer>( Integer.class, numElements);
-		
-		for (int index = 0; index< numElements;index++){
-			bs.set(index, randInt(0,numElements));
+
+	public static void main(String[] args) {
+		int numElements = (int) Math.pow(10, 5);
+		BubbleSort<Integer> bs = new BubbleSort<Integer>(Integer.class, numElements);
+		// populate the array
+		for (int index = 0; index < numElements; index++) {
+			bs.set(index, randInt(0, numElements));
 		}
-		
-		bs.print();
+		bs.print();// print the array
 		double start = System.currentTimeMillis();
-		bs.sort();
+		bs.sort();// sort the array
 		double stop = System.currentTimeMillis();
-		bs.print();
-		System.out.println(stop-start);
+		bs.print();// print the sorted array
+		System.out.println(stop - start);// print the execution time
 	}
 
+	/**
+	 * Random Int generator for a specified range
+	 * @param min
+	 * @param max
+	 * @return
+	 */
 	public static int randInt(int min, int max) {
-
-	    // NOTE: This will (intentionally) not run as written so that folks
-	    // copy-pasting have to think about how to initialize their
-	    // Random instance.  Initialization of the Random instance is outside
-	    // the main scope of the question, but some decent options are to have
-	    // a field that is initialized once and then re-used as needed or to
-	    // use ThreadLocalRandom (if using at least Java 1.7).
-	    Random rand = new Random();
-
-	    // nextInt is normally exclusive of the top value,
-	    // so add 1 to make it inclusive
-	    int randomNum = rand.nextInt((max - min) + 1) + min;
-
-	    return randomNum;
+		Random rand = new Random();
+		// nextInt is normally exclusive of the top value,
+		// so add 1 to make it inclusive
+		int randomNum = rand.nextInt((max - min) + 1) + min;
+		return randomNum;
 	}
-	
-	public void sort(){
-	    	int indexCur = 0, indexStop = a.length-1, indexLastChange = 0;
-	    	E eCur, eNext;
-	    	while(indexCur<indexStop){
-	    		while(indexCur<indexStop){
-	    			eCur = a[indexCur];
-	    			eNext = a[indexCur+1];
-	    			if(eCur.compareTo(eNext)>0){
-	    				a[indexCur] = eNext;
-	    				a[indexCur+1] = eCur;
-	    				indexLastChange = indexCur;
-	    			}
-	    			indexCur++;
-	    		}
-	    		indexCur = 0;
-	    		indexStop = Math.min(indexStop-1, indexLastChange);
-	    	}
-	    }
 
+	/**
+	 * This function sorts the ascending order
+	 */
+	public void sort() {
+		int indexCur = 0, indexStop = a.length - 1, indexLastChange = 0;
+		E eCur, eNext;
+		while (indexCur < indexStop) {
+			while (indexCur < indexStop) {
+				eCur = a[indexCur];
+				eNext = a[indexCur + 1];
+				if (eCur.compareTo(eNext) > 0) {
+					a[indexCur] = eNext;
+					a[indexCur + 1] = eCur;
+					indexLastChange = indexCur;
+				}
+				indexCur++;
+			}
+			indexCur = 0;
+			indexStop = Math.min(indexStop - 1, indexLastChange);
+		}
+	}
+
+	/**
+	 * Prints to system.out the array
+	 */
 	public void print() {
 		for (int index = 0; index < a.length; index++) {
 			System.out.printf("%s, ", a[index].toString());
@@ -73,10 +80,21 @@ public class BubbleSort<E extends Comparable<E>> {
 		System.out.println();
 	}
 
+	/**
+	 * Returns the element of the array at the specified index
+	 * @param index the position we wish to return
+	 * @return the element at the index specified
+	 */
 	E get(int index) {
 		return a[index];
 	}
 
+	/**
+	 * Sets the element of the array at the position index to be equal to the element provided
+	 * @param index the index of the position we want to set
+	 * @param element The element we want to set the array at the position of the index 
+	 * @return the element at the index specified
+	 */
 	E set(int index, E element) {
 		return a[index] = element;
 	}
